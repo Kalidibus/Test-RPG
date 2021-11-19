e78c114586971cebfd45f70f80bdaa577443eacd
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2021-11-19 13:23:03 -0500

    Update skill names

--- Data/Classes/Fist.gd
+++ Data/Classes/Fist.gd
@@ -2,8 +2,8 @@ extends Entity
 
 func _ready():
 	charname = "Fist"
-	MaxHP = 50
-	HP = 50
+	MaxHP = 100
+	HP = 100
 	MaxMP = 80
 	MP = 80
 	STR = 70
@@ -15,16 +15,16 @@ func _ready():
 	emit_signal("display_stats", charname,HP,MP,MaxHP,MaxMP, row)
 	
 	skilllist = {
-		"Violet_Fist": "Strikes an opponent with a flurry of blows. Scales with SPD and STR",
-		"Bastard_Spirit" : "Raises STR for 3 turns",
-		"Lunatic_Spirit" : "Greatly raises STR and SPD for 3 turns, but reduces DEF"
+		"Violet Fist": "Strikes an opponent with a flurry of blows. Scales with SPD and STR",
+		"Bastard Spirit" : "Raises STR for 3 turns",
+		"Lunatic Spirit" : "Greatly raises STR and SPD for 3 turns, but reduces DEF"
 	}
 
-func Violet_Fist():
+func VioletFist():
 	if MPCheck(40) == "fail": return
-	else: CombatGUI.TargetList("Violet_Fist2")
+	else: CombatGUI.TargetList("VioletFist2")
 
-func Violet_Fist2(target):
+func VioletFist2(target):
 	var damage = (STR/50+SPD/25)*calcdamage(self, target)
 	target.take_damage(damage)
 	target.take_damage(damage)
@@ -34,7 +34,7 @@ func Violet_Fist2(target):
 	
 	CloseTurn(str(charname) + " has attacked " + str(target.charname) + " for " + str(damage) + " damage!")
 
-func Bastard_Spirit():
+func BastardSpirit():
 	if MPCheck(20) == "fail": return
 	
 	MPCost(20)
@@ -43,7 +43,7 @@ func Bastard_Spirit():
 	
 	CloseTurn(str(charname) + " draws upon her Bastard Spirit. STR Increased.")
 
-func Lunatic_Spirit():
+func LunaticSpirit():
 	if MPCheck(20) == "fail": return
 	
 	MPCost(20)
