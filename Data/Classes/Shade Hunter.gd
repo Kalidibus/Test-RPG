0930561a8d297ade4ad706188069953937c0d68a
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2021-11-29 15:52:44 -0500

    Added Shade Hunter abilities
    (untested)

--- Data/Classes/Shade Hunter.gd
+++ Data/Classes/Shade Hunter.gd
@@ -2,8 +2,8 @@ extends Entity
 
 func _ready():
 	charname = "Shade Hunter"
-	MaxHP = 75
-	HP = 75
+	MaxHP = 60
+	HP = 60
 	MaxMP = 60
 	MP = 60
 	STR = 50
@@ -15,26 +15,62 @@ func _ready():
 	emit_signal("display_stats", charname,HP,MP,MaxHP,MaxMP, row)
 	
 	skilllist = {
-	"Poison Arrow" : "An arrow dipped in Raklak poison secretions",
-	"Burning Arrow" : "An arrow dipped in molten inferno"
+	"Poison Arrow" : "An arrow dipped in Raklak poison secretions. Can inflict Poison.",
+	"Burning Arrow" : "An arrow dipped in molten inferno. Can inflict Burn.",
+	"Bladed Volley" : "Slashing damage to all back line enemies.",
+	"Plate Crusher" : "A single target impact based shot with a chance to reduce enemies DEF.",
+	"Isolate Prey" : "Target weak links in the enemies lines. Moderate piercing damage that scales with SPD. Deal bonus damage for each status ailment on target."
 	}
 
 func PoisonArrow():
 	if MPCheck(10) == "fail": return
 	CombatGUI.TargetList("PoisonArrow2")
 func PoisonArrow2(target):
-	var damage = 0.5*STR + 0.5*SPD
-	target.take_damage(damage, "pierce")
+	var damage = (SPD * statmods["SPD"])*0.5
+	var adjusteddamage = target.take_damage(damage, "pierce")
 	MPCost(10)
 	CloseTurn(str(charname) + " fires a poison drenched arrow at " + str(target.charname) + ", hitting it for " + str(damage) + " damage!")
-	target.AttemptStatusAilment("poison", 70, 2)
+	target.AttemptStatusAilment("poison", 40, 3)
 
 func BurningArrow():
 	if MPCheck(10) == "fail": return
 	CombatGUI.TargetList("BurningArrow2")
 func BurningArrow2(target):
-	var damage = 0.5*STR + 0.5*SPD
-	target.take_damage(damage, "pierce")
+	var damage = (SPD * statmods["SPD"])*0.5
+	var adjusteddamage = target.take_damage(damage, "pierce")
 	MPCost(10)
-	CloseTurn(str(charname) + " fires a flaming arrow at " + str(target.charname) + ", hitting it for " + str(damage) + " damage!")
+	CloseTurn(str(charname) + " fires a flaming arrow at " + str(target.charname) + ", hitting it for " + str(adjusteddamage) + " damage!")
+	target.AttemptStatusAilment("burn", 40, 3)
+	
+func BladedVolley():
+	if MPCheck(30) == "fail": return
+	var damage = (SPD * statmods["SPD"])
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	MPCost(30)
+	EventHandler.BattleLog(str(charname) + " fires a Bladed Volley!")
+	for target in enemies:
+		if row == "Back":
+			var adjusteddamage = target.take_damage(damage, "slash")
+			EventHandler.BattleLog(str(target.charname) + " is hit for " + str(adjusteddamage) + " damage!")
+	CloseTurn("")
+
+func PlateCrusher():
+	if MPCheck(20) == "fail": return
+	CombatGUI.TargetList("PlateCrusher2")
+func PlateCrusher2(target):
+	var damage = (SPD * statmods["SPD"])
+	var adjusteddamage = target.take_damage(damage, "impact")
+	MPCost(20)
+	CloseTurn(str(charname) + " fires a flaming arrow at " + str(target.charname) + ", hitting it for " + str(adjusteddamage) + " damage!")
 	target.AttemptStatusAilment("burn", 70, 2)
+	var rng = RNG()
+	if rng >= 50: target.StatMod("DEF", 0.6, 2)
+
+func IsolatePrey():
+	if MPCheck(20) == "fail": return
+	CombatGUI.TargetList("IsolatePrey2")
+func IsolatePrey2(target):
+	var damage = max((SPD * statmods["SPD"])*0.5, (SPD * statmods["SPD"]) * target.status.count())
+	var adjusteddamage = target.take_damage(damage, "pierce")
+	MPCost(20)
+	CloseTurn(str(charname) + " strikes at the vitals of the weakened " + str(target.charname) + ", hitting it for " + str(adjusteddamage) + " damage!")
