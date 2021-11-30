3751b94fb7ec3929f03452dfe1a8bde1cb189cab
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2021-11-30 13:05:20 -0500

    fix

--- Data/Classes/Dancer.gd
+++ Data/Classes/Dancer.gd
@@ -19,8 +19,13 @@ func _ready():
 	
 	skilllist = {
 		"Raptor Samba" : "Increases Party-wide Strength",
+<<<<<<< HEAD
 	"Soothing Waltz" : "Applies a regen effect to the whole party.",
 	"Invigorating Galliard" : "Increases Party-wide Speed"
+=======
+    "Soothing Waltz" : "Applies a regen effect to the whole party.",
+    "Invigorating Galliard" : "Increases Party-wide Speed"
+>>>>>>> parent of 3848970 (Bug Fixes)
 	}
 
 func RaptorSamba():
@@ -28,7 +33,11 @@ func RaptorSamba():
 	var partylist = get_tree().get_nodes_in_group("partymembers")
 	MPCost(30)
 	for n in partylist:
+<<<<<<< HEAD
 	if n.HP != 0: StatMod("STR", 1.25, 2)
+=======
+    if n.HP != 0: StatMod("STR", 1.25, 2)
+>>>>>>> parent of 3848970 (Bug Fixes)
   CloseTurn(str(charname) + "'s Raptor Samba emboldens the party. Strength increased!")
   
 func SoothingWaltz():
@@ -36,7 +45,11 @@ func SoothingWaltz():
 	var partylist = get_tree().get_nodes_in_group("partymembers")
 	MPCost(30)
 	for n in partylist:
+<<<<<<< HEAD
 	if n.HP != 0: AttemptStatusAilment("regen", 30, 2)
+=======
+    if n.HP != 0: AttemptStatusAilment("regen", 30, 2)
+>>>>>>> parent of 3848970 (Bug Fixes)
   CloseTurn(str(charname) + "'s Soothing Waltz begins healing the party!")
    
 func InvigoratingGalliard():
@@ -44,5 +57,9 @@ func InvigoratingGalliard():
 	var partylist = get_tree().get_nodes_in_group("partymembers")
 	MPCost(30)
 	for n in partylist:
+<<<<<<< HEAD
 	if n.HP != 0: StatMod("SPD", 1.25, 2)
+=======
+    if n.HP != 0: StatMod("SPD", 1.25, 2)
+>>>>>>> parent of 3848970 (Bug Fixes)
   CloseTurn(str(charname) + "'s Invigorating Galliad motivates the party. Speed increased!")
