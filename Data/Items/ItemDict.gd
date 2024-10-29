e5ca2da17752c2b0856ca3625345e6ffa4617e1f
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-10-29 13:57:41 -0400

    Create ItemDict.gd

---
+++ Data/Items/ItemDict.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var swords = preload(%Swords)
+@onready var axes = preload(%Axes)
+@onready var bows = preload(%Bows)
+@onready var staves = preload(%Staves)
+@onready var hammers = preload(%Hammers)
+@onready var arcana = preload(%Arcana)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
