3bd7e67487d13ae066fce94f3354bcd1a364159f
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-10-29 14:06:38 -0400

    Create Arcana.gd

---
+++ Data/Items/Arcana.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var dict = {
+	"ws01" = {
+		"name" = "Corroded Arcana",
+		"stats" = {"FTH" = 2},
+		"desc" = "Was once a formidable Arcana. Broken down and decayed by the Brine, now only fragments remains.",
+		"value" = 5
+	}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
