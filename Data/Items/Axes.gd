327ceff7412725a44dfa5cf65b4124336778f534
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-10-29 14:05:56 -0400

    Create Axes.gd

---
+++ Data/Items/Axes.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var dict = {
+	"wa01" = {
+		"name" = "Corroded Axe Hilt",
+		"stats" = {"STR" = 2},
+		"desc" = "Was once a formidable axe. Broken down and decayed by the Brine, now only now only a hilt remains.",
+		"value" = 5
+	}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
