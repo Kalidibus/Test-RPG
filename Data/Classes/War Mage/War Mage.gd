9a2b1f7c38fc6503b3c12d88b9f12d850963be15
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-10-29 10:53:40 -0400

    Update War Mage.gd

--- Data/Classes/War Mage/War Mage.gd
+++ Data/Classes/War Mage/War Mage.gd
@@ -12,7 +12,7 @@ var starting_stats = {
 		"SPD" = 25
 		}
 
-var job_description = "TBD"
+var job_description = "The War Mage is a frontline caster with explosive capabilities. \n\nSpecializing in close-range Levin and Infernal damage, they are monsters against groups of enemies that fight in the front-line. Hitting enemies with Infernal or Levin damage also accumulates a resource called [b]Overheat[/b] which can be used to fuel explosive attacks that hit the whole enemy party. \n\nThier capabilities against single enemies, or ranged enemies can be found lacking."
 
 func _ready():
 	charname = "War Mage"
