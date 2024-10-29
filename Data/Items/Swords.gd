925b757966cbbd9bd40ea0dcf1a53c6f9e20b44c
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-10-29 13:59:22 -0400

    Create Swords.gd

---
+++ Data/Items/Swords.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var dict = {
+	"ws01" = {
+		"name" = "Corroded Sword Hilt",
+		"stats" = {"STR" = 2},
+		"desc" = "Was once a formidable sword. Broken down and decayed by the Brine, now only a hilt remains.",
+		"value" = 5
+	}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
