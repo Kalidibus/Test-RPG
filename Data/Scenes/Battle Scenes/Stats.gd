1e6cda73438ec174d8e8d33d1388a036bd65537b
Kalidibus <93565603+Kalidibus@users.noreply.github.com>
2024-11-14 13:41:13 -0500

    Create Stats.gd

---
+++ Data/Scenes/Battle Scenes/Stats.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name Stats
+
+enum stat {HP, HPmax, MP, MPmax, STR, DEX, DEF, RES, INT, FTH, EVD, ACC, SPD, HATE}
+enum damage_type {PIERCE, IMPACT, SLASH, INFERNAL, LEVIN, DEEP, ERDE, VIRTUOS, FEL}
+enum status_effects {POISON, BURN, BLIND, STUN, SEAL, MARKED, REGEN}
+
+func _ready():
+	pass
